4569175b09b2d3462df4574ff66631ee2ab11119
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2020-06-07 21:22:15 -0300

    [FEAT]: Ultima aula

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,6 @@
 import express, { response } from 'express';
+import { celebrate, Joi } from 'celebrate';
+
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -22,7 +24,26 @@ delete - Deleta um recurso
 routes.get('/items', ItemsController.index);
 
 // Create points 
-routes.post('/points', upload.single('image'), PointsController.create);
+routes.post('/points', 
+            upload.single('image'), 
+            celebrate({
+                body: Joi.object().keys({
+                    name: Joi.string().required(),
+                    email: Joi.string().required().email(),
+                    whatsapp: Joi.number().required(),
+                    latitude: Joi.number().required(),
+                    longitude: Joi.number().required(),
+                    city: Joi.string().required(),
+                    uf: Joi.string().required().max(2),
+                    items: Joi.string().required()
+                })
+            }, {
+                // Esse parametro indica que devem ser validados todos os campos de uma vez
+                abortEarly: false
+            }),
+            PointsController.create);
+
+
 // List points with filters
 routes.get('/points', PointsController.index);
 // List one point
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import routes from './routes';
 import path from 'path';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,8 @@ app.use(routes);
 
 // Serve arquivos estaticos a partir de uma pasta 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333, () => {
     console.log(`Server is running on port: 3333`);
